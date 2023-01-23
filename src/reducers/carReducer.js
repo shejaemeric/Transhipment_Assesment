baba417e583f8381f9879ac2ca5bc4bd28fb38af
shejaemeric2 <shejaemeric2@gmail.com>
2023-01-23 12:30:27 +0200

    remove redudant

--- src/reducers/carReducer.js
+++ src/reducers/carReducer.js
@@ -1,7 +1,6 @@
 import { ADD_CAR } from "../constants/actionTypes";
 
 const carReducer = (state = [], action) => {
-  console.log(state);
   switch (action.type) {
     case ADD_CAR:
       return [...state, action.payload];
